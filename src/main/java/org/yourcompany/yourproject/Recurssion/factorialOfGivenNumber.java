f486ee8c34e347f20b79d00cee1cb1c82494eee4
neeraj3161 <neerajtripathi7447@gmail.com>
2026-09-02 21:55:30 +0530

    Refactor factorial and sum methods; update main method to demonstrate factorial calculation

--- src/main/java/org/yourcompany/yourproject/Recurssion/factorialOfGivenNumber.java
+++ src/main/java/org/yourcompany/yourproject/Recurssion/factorialOfGivenNumber.java
@@ -3,15 +3,26 @@ package org.yourcompany.yourproject.Recurssion;
 public class factorialOfGivenNumber {
     public static void main(String[] args)
 {
-    System.out.println("Factorial of given number: "+factorial(5));
+    //System.out.println("Sum of given number: "+sum(5));
+    System.out.println("Factorial of  given number: "+factorial(2));
+}
+
+public static int sum(int n)
+{
+    if(n==0)
+    {
+        return 0;
+    }
+    return n+sum(n-1);
 }
 
 public static int factorial(int n)
 {
     if(n==0)
     {
-        return 0;
+        return 1;
     }
-    return n+factorial(n-1);
+
+    return n*factorial(n-1);
 }
 }
